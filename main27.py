d8eed7315f401132f6a432a13cd2b602bf07755f
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-16 21:39:36 +0300

    какой-то косяк с re.split()

---
+++ main27.py
@@ -0,0 +1,36 @@
+﻿# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the -sea shore The -shells
+# that she -sells are -sea -shells I'm sure.So if -she -sells -sea
+# -shells -on -the -sea -shore I'm sure -that -the -shells -are -sea
+# -shore -shells
+# Output: 13
+import re
+
+print()
+
+inp_str = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+inp_str = inp_str.lower()
+print('получено: ', inp_str)
+#inp_list = inp_str.split()
+#inp_list = inp_str.split('.')
+#inp_list = inp_str.split("'")
+inp_list = re.split(" . | ' | , ", inp_str)
+print('обработано: ', inp_list)
+dict = {}
+for i in range(len(inp_list)):
+    print(inp_list[i])
+    if inp_list[i] in dict:
+        dict[inp_list[i]] += 1
+    else:
+        dict[inp_list[i]] = 1
+    #print('{} : {}'.format(i, dict[i]))    
+for i in dict:
+    print('{} : {}'.format(i, dict[i]))
+#print(end = '\n')
+print(len(dict))
