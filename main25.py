a37ff9e8c50179508bd7705c7cf998eb6f7b18d7
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-04-17 23:00:49 +0300

    task25 added

---
+++ main25.py
@@ -0,0 +1,31 @@
+﻿# Задача №25. Решение в группах
+# Напишите программу, которая принимает на вход
+# строку, и отслеживает, сколько раз каждый символ
+# уже встречался. Количество повторов добавляется к
+# символам с помощью постфикса формата _n.
+# Input: a a a b c a a d c d d
+# Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+
+print()
+first_str = 'a a a b c a a d c d d'
+list_str = list(first_str.split())
+print(first_str)
+my_dict = {}
+
+for i in range(len(list_str)):
+    if list_str[i] in my_dict:
+        my_dict[list_str[i]] += 1
+        list_str[i] += '_' + str(my_dict[list_str[i]])
+    else:
+        my_dict[list_str[i]] = 1
+        list_str[i] += '_' + str(my_dict[list_str[i]])
+        
+print('--------------------------------------------')
+for i in range(len(list_str)):
+    print(list_str[i], end = " ")
+print( end = '\n')
+print('--------------------------------------------')
+print()
+
+
+
--- main27.py
+++ main27.py
@@ -9,28 +9,26 @@
 # shells on the sea shore I'm sure that the shells are sea
 # shore shells
 # Output: 13
-import re
 
 print()
 
 inp_str = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure. So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
 
 inp_str = inp_str.lower() # сбрасываем все буквы в нижний регистр
-#print('получено: ', inp_str)
-#inp_list = inp_str.split()
+
 inp_list = inp_str.split('.') # избавляемся от точек, создав список из строк без точек
-#out_list = []
+
 temp_str = ''
 print()
 
 for i in range(len(inp_list)):
     temp_str += inp_list[i]
-#print(temp_str)
+
 inp_list = temp_str.split()
 
 dict = {}
 for i in range(len(inp_list)):
-    #print(inp_list[i])
+    
     if inp_list[i] in dict:
         dict[inp_list[i]] += 1
     else:
